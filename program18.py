6d998d55ff30bc52b679459e7e4ee5dca81336f7
AsfarButt <asfarbutt69@gmail.com>
2026-01-27 00:08:16 +0500

    Update 8

--- program18.py
+++ program18.py
@@ -3,6 +3,35 @@ import numpy as np
 
 dataset = pd.read_csv("practicedataset2.csv")
 
-dataset["OnTime"] = (dataset["Delivered"] == "Yes") & (dataset["DeliveryDays"] <= 4)
-ontime_per_city =  dataset.groupby("City")["OnTime"].sum()
-print(ontime_per_city)
+# dataset["OnTime"] = (dataset["Delivered"] == "Yes") & (dataset["DeliveryDays"] <= 4)
+# on_time_per_city = dataset.groupby("City")["OnTime"].mean().mul(100)
+# print(on_time_per_city.round(1))
+
+# dataset["OrderDate"] = pd.to_datetime(dataset["OrderDate"])
+# print(dataset["OrderDate"].dtype)
+# dataset["OrderMonth"] = dataset["OrderDate"].dt.month
+# # print(dataset[["OrderID","OrderMonth"]])
+# # OrderDetails = pd.DataFrame()
+# # OrderDetails["TotalAmount"] = dataset.groupby("OrderMonth")["OrderAmount"].sum()
+# # print(OrderDetails)
+# # OrderDetails["NoOfOrders"] = dataset.groupby("OrderMonth")["OrderID"].count()
+# # print(OrderDetails)
+# # OrderDetails["AvgOrderAmount"] = dataset.groupby("OrderMonth")["OrderAmount"].median()
+# # print(OrderDetails)
+
+# OrderDetails = (
+#     dataset.groupby("OrderMonth").agg(
+#         TotalAmount = ("OrderAmount","sum"),
+#         NoOfOrders = ("OrderID","count"),
+#         AvgOrderAmount = ("OrderAmount","median")
+#     )
+# )
+
+# print(OrderDetails)
+# print(OrderDetails.dtypes)
+
+
+CustomerDetails = dataset.groupby(["City","Customer"])["OrderAmount"].sum()
+print(CustomerDetails)
+
+
