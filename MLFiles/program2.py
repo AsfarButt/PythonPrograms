0b681e914c630b6ce1a8c05579c227df307deedd
AsfarButt <asfarbutt69@gmail.com>
2026-02-07 20:03:35 +0500

    Update 3:

--- MLFiles/program2.py
+++ MLFiles/program2.py
@@ -4,10 +4,17 @@ from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_absolute_error
 
-dataset1 = pd.read_csv("practicedataset1.csv")
-dataset2 = pd.read_csv("practicedataset2.csv")
-dataset3 = pd.read_csv("practicedataset3.csv")
+dataset1 = pd.read_csv("W:\Work\Python-Files\MLFiles\practicedataset1.csv")
+dataset2 = pd.read_csv("W:\Work\Python-Files\MLFiles\practicedataset2.csv")
+dataset3 = pd.read_csv("W:\Work\Python-Files\MLFiles\practicedataset3.csv")
 
 dataset = pd.concat([dataset1, dataset2, dataset3], axis=0)
 
-print(dataset)
+X = dataset[["years_experience", "education_level", "age", "weekly_hours", "projects_completed"]]
+
+Y = dataset[["annual_salary"]]
+
+MLModel = LinearRegression()
+
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
+
